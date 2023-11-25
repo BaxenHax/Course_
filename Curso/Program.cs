3522e59298b1c899326aa659da61c45b003fb69f
Rodrigo Sartori <rodrigo.sartori1544@gmail.com>
2023-11-25 20:58:21 -0300

    Aula Enum 1

--- Curso/Program.cs
+++ Curso/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Curso.Entities;
+using Curso.Entities.Enums;
+using System;
+
+namespace Curso
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                ID = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+            Console.WriteLine(order);
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
+        }
+
+        
+    }
+}
