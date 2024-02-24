76151db5496fbe60cacc50d7995dd04ed6fb9c57
Rodrigo Sartori <rodrigo.sartori1544@gmail.com>
2024-02-24 17:24:12 -0300

    HERANÇA E POLIMORFISMO: DOWNCASTNG E UPCASTING

--- Curso/Program.cs
+++ Curso/Program.cs
@@ -9,8 +9,34 @@ namespace Curso
     {
         static void Main(string[] args)
         {
-            BusinessAccount account = new BusinessAccount(8010, "BobBrown", 100.0, 500.0);
-            Console.WriteLine(account.Balance);
+            Account acc = new Account(1001, "Alex", 0.0);
+            BusinessAccount acc2 = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+            //UPCASTING
+
+            Account acc1 = acc2;
+            Account bacc = new BusinessAccount(1003, "bob", 0.0, 300.0);
+            Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 500.0);
+
+            //DOWNCASTING
+
+            BusinessAccount acc4 = (BusinessAccount)acc2;
+            acc4.Loan(100.0);
+
+            //BusinessAccount acc5 = (BusinessAccount)acc3
+            if (acc3 is BusinessAccount)
+            { 
+                BusinessAccount acc5 = (BusinessAccount)acc3;
+                acc5.Loan(200.0);
+                Console.WriteLine("Loan!");
+            }
+
+            if (acc3 is SavingsAccount)
+            {
+                SavingsAccount acc5 = (SavingsAccount) acc3;
+                acc5.UpdateBalance();
+                Console.WriteLine("Update!");
+            }
 
             
         }
